e66ffb90b02a105e8308a65d7c5ad0695e5f6278
nouran <nouranhaytham80@gmail.com>
2024-07-30 22:39:48 +0200

    new

--- pong.py
+++ pong.py
@@ -1,12 +1,17 @@
 import turtle
 turtle.Turtle()
+wn=turtle.Screen()
+wn.title("pong game")
+wn.bgcolor("white")
+wn.tracer(0)
+
 
 def paddle_a():
     paddle_a=turtle.Turtle()
     paddle_a.penup()      #so it doesn't trace the drawing of the square
     paddle_a.shape("square")
     paddle_a.goto(-350,0)
-    paddle_a.color("black")
+    paddle_a.color("blue")
     paddle_a.shapesize(stretch_len=1,stretch_wid=5)
     paddle_a.speed(0)
 
@@ -15,24 +20,17 @@ def paddle_b():
     paddle_b.penup()      #so it doesn't trace the drawing of the square
     paddle_b.shape("square")
     paddle_b.goto(350,0)
-    paddle_b.color("black")
+    paddle_b.color("red")
     paddle_b.shapesize(stretch_len=1,stretch_wid=5)
     paddle_b.speed(0)
 
 def ball():
-    ball=turtle.Turtle
-    ball.penup()
+    ball=turtle.Turtle()
+    ball.penup()      #so it doesn't trace the drawing of the square
     ball.shape("circle")
-    ball.color("red")
-    ball.shapesize(2)
-    ball.speed(0)
     ball.goto(0,0)
-paddle_a()
-paddle_b()
-ball()
-
-
-
+    ball.color("black")
+    ball.speed(0)
 
 def paddle_a_up():
     y = paddle_a.ycorr()
@@ -43,11 +41,11 @@ def paddle_a_down():
     y =paddle_a.ycore()
     y -= 20
 
-
+paddle_a()
+paddle_b()
+ball()
 wn.listen()
 wn.onkeypress(paddle_a_up,  "w")
 wn.onkeypress(paddle_a_down,  "w")
-
-
-
-
+while True:
+    wn.update()
